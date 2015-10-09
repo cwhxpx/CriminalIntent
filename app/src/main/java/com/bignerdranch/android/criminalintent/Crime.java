04d6c7b147f333b71a642be2bd86964e2f892026
cwh <cwh.xpx@gmail.com>
2015-10-09 16:04:07 +1100

    为新的model类增加mId和mTitle并getter和setter方法

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,7 +1,28 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.UUID;
+
 /**
  * Created by cwh on 10/7/15.
  */
 public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime(){
+        //Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
 }
