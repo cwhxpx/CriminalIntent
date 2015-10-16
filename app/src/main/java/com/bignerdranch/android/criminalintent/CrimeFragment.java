bbc5ac1f2d7b05449937d1949a6926c1b9b56673
cwh@mba <cwh.xpx@gmail.com>
2015-10-17 06:29:00 +1100

    1.增加fragment标题成员变量 2.标题成员变量标与标题文本编辑框控件关联 3.标题文本编辑框控件添加输入监听

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -2,15 +2,19 @@ package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by cwh on 10/16/15.
  */
 public class CrimeFragment extends Fragment{
     private Crime mCrime;
+    private EditText mTitleField;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -22,6 +26,25 @@ public class CrimeFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        //wire-up widget to response user's operations
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //This space intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setmTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //This space intentionally left blank
+            }
+        });
         return v;
     }
 }
