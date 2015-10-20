429836947af33f1e2487573322c2f983c495475c
cwh@mba <cwh.xpx@gmail.com>
2015-10-20 16:02:52 +1100

    新的fragment list

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by cwh on 20/10/2015.
+ */
+public class CrimeListFragment extends Fragment{
+}
