ae6cdf627ac7b7158ce421c73f5f237d7379a1aa
cwh <cwh.xpx@gmail.com>
2015-10-27 08:53:24 +1100

    新增加日期选择控件

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,24 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+
+/**
+ * Created by cwh on 10/26/15.
+ */
+public class DatePickerFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstancedState){
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
