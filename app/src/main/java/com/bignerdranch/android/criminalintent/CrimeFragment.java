41e8327733ef542ab658a740d1d414338006363e
cwh <cwh.xpx@gmail.com>
2015-11-03 13:25:57 +1100

    事先验证即将发出的隐式intent是否有对应的activity

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.criminalintent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -133,6 +134,7 @@ public class CrimeFragment extends Fragment{
 
         final Intent pickContact = new Intent(Intent.ACTION_PICK,
                 ContactsContract.Contacts.CONTENT_URI);
+//        pickContact.addCategory(Intent.CATEGORY_HOME);
         mSuspectButton = (Button)v.findViewById(R.id.crime_suspect);
         mSuspectButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -142,6 +144,12 @@ public class CrimeFragment extends Fragment{
 
         if(mCrime.getSuspect() != null){
             mSuspectButton.setText(mCrime.getSuspect());
+        }
+
+        PackageManager pm = getActivity().getPackageManager();
+        if(pm.resolveActivity(pickContact,
+                PackageManager.MATCH_DEFAULT_ONLY) == null){
+            mSuspectButton.setEnabled(false);
         }
 
         return v;
